9b6c0ebd2cb21b768884353d6ab03af263e89f29
youssefbassem42 <youssefbassem42@gmail.com>
2024-10-11 00:48:06 +0300

    edit

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
-''' sum list function '''
+'''Task 5's module.
+'''
+from typing import List
 
-def sum_list(input_list : list[float]) -> float:
-    ''' 
-    Calculate the sum of a list of floating-point numbers.
-
-    Parameters:
-    input_list (list[float]): A list of floating-point numbers to be summed.
-
-    Returns:
-    float: The sum of the input list as a floating-point number.
+def sum_list(input_list: List[float]) -> float:
+    '''Computes the sum of a list of floating-point numbers.
     '''
     return float(sum(input_list))
